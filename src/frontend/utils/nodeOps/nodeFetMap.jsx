8f7d4fc0bb591a782daeec35d090143a7260cb95
jaiminchauhan <48099245+Jaimin020@users.noreply.github.com>
2025-02-07 23:05:08 +0530

    Update Parameter Viewer
    Added functionality to import new node information via an XML file, which will be reflected in the parameter viewer on the frontend.

---
+++ src/frontend/utils/nodeOps/nodeFetMap.jsx
@@ -0,0 +1,37 @@
+import { XMLParser } from 'fast-xml-parser';
+
+export const getNodeFeatureMap = async (xmlFileName) => {
+  const nodeFeatureMap = new Map();
+
+  // Read XML file
+  const xmlData = await window.file.readFile(xmlFileName);
+
+  // Initialize XML parser
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "",
+    allowBooleanAttributes: true,
+    parseAttributeValue: true,
+    parseTagValue: true,
+    trimValues: true
+  });
+  const result = parser.parse(xmlData);
+  console.log(result);
+
+  // Extract node names and features
+  if (result.nodes && result.nodes.node) {
+    result.nodes.node.forEach(node => {
+      const nodeInfo = {
+        name: node.name,
+        feature: node.feature,
+        library: node.library,
+        codeId: node.codeId,
+        parameters: node.parameters.param,
+        code: node.code
+      };
+      nodeFeatureMap.set(node.name, nodeInfo);
+    });
+  }
+  
+  return nodeFeatureMap;
+};
